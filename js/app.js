d8dc2a0e283d00bad2c357503c32125a8bad92b6
System Administrator <root@Agastya-Darma-MacBook-Pro.local>
2016-05-28 02:09:27 +0700

    First Commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+
+var app = angular.module("AgastyaBlog", ['ui.bootstrap']);
+var app = angular.module("AgastyaBlog", ['ngSanitize']);
+
+
+app.controller("PostsCtrl", function($scope, $http) {
+  $http.get('https://www.agastyadarma.com/api/wp-json/wp/v2/posts/?per_page=1').
+    success(function(data, status, headers, config) {
+      $scope.posts = data;
+      
+    }).
+    error(function(data, status, headers, config) {
+      // log error
+    });
+});
