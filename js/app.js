b2b7746a7d52bf537fc1a177f3e0aa25b5e082e2
System Administrator <root@Agastya-Darma-MacBook-Pro.local>
2016-06-14 03:29:38 +0700

    a

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var app = angular.module("AgastyaBlog", ['ngSanitize']);
 
 
 app.controller("PostsCtrl", function($scope, $http) {
-  $http.get('https://www.agastyadarma.com/api/wp-json/wp/v2/posts/?per_page=1').
+  $http.get('https://www.agastyadarma.com/api/blog/wp-json/wp/v2/posts/?per_page=1').
     success(function(data, status, headers, config) {
       $scope.posts = data;
       
